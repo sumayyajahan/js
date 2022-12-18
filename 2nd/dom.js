100be0b2b754fa4561987ef9b9f17396e03e7f99
sumayyayellow <sumayyayellow@gmail.com>
2022-12-18 15:35:18 -0600

    2nd code

--- 2nd/dom.js
+++ 2nd/dom.js
@@ -1,13 +1,3 @@
-Array.prototype.myPush = function(...a){
-   this.push(a[0]);
-   init();
-}
-
-const display = document.getElementById("fruits");
-const button = document.querySelector("#button");
-
-let fruits = ['mango', 'guava', 'apple', 'orange'];
-
-const init = function(){
-    
-}
+let array = [];
+increment = 0;
+let container = document.querySelector('.container');
